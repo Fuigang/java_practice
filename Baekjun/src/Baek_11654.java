2b074ceebc6fbab1317b94f879af603ae6f82bdf
Fuigang <144595322+Fuigang@users.noreply.github.com>
2025-06-18 10:13:23 +0900

    Add files via upload

---
+++ Baekjun/src/Baek_11654.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baek_11654 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.next();
+		int ch =  input.charAt(0);
+		
+		
+		
+		
+		System.out.println(ch);
+		
+	}
+}
