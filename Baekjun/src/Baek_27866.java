44543bbaf156fc23b1537e50fa60223d2e1b4fd0
Fuigang <144595322+Fuigang@users.noreply.github.com>
2025-06-18 17:14:26 +0900

    Add files via upload

---
+++ Baekjun/src/Baek_27866.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Baek_27866 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String word = sc.nextLine();
+		int num  = sc.nextInt();
+		
+		System.out.println(word.charAt(num-1));
+		
+		
+		
+		
+		
+		
+	}
+}
